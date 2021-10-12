1ccaf280253463d02bef307c2be0e2e13c4e38c8
iheb_ishak <ihebishak8@gmail.com>
2021-10-12 03:44:08 +0100

    checkoutService_and_checkoutController

--- src/main/java/com/project/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/project/ecommerce/service/CheckoutServiceImpl.java
@@ -3,7 +3,14 @@ package com.project.ecommerce.service;
 import com.project.ecommerce.dao.CustomerRepository;
 import com.project.ecommerce.dto.Purchase;
 import com.project.ecommerce.dto.PurchaseResponse;
+import com.project.ecommerce.entity.Customer;
+import com.project.ecommerce.entity.Order;
+import com.project.ecommerce.entity.OrderItem;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -15,7 +22,38 @@ public class CheckoutServiceImpl implements CheckoutService{
 
     }
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+
+        //retrieve the order info from dto
+        Order order =purchase.getOrder();
+        //generate tracking number
+        String orderTrackingNumber=generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+
+        //populate order with orderItems
+        Set<OrderItem> orderItems=purchase.getOrderItems();
+        orderItems.forEach(item->order.add(item));
+
+        //populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        //populate customer with order
+        Customer customer= purchase.getCustomer();
+        customer.add(order);
+
+        //save to the database
+        customerRepository.save(customer);
+
+        //return a response
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber(){
+
+
+        return UUID.randomUUID().toString();
     }
 }
